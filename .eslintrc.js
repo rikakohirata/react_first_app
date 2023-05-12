c1e2ae1465133c60875925888fa749cf362d4115
rikakohirata <rikako.hirata88@gmail.com>
2023-05-12 15:00:12 +0900

    ESLintをテスト

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  // チェック対象のJavaScript/TypeScriptコードが、
+  // どの実行環境で使われるかをESLintに伝えるためのオプション
+  env: {
+    browser: true,  // windowやalertなどのグローバル変数が認識される
+    es2021: true,   // ES2021までに導入されたグローバル変数が認識される
+  },
+  // チェック対象のJavaScriptが,
+  // どの構文を使っているかをESLintに伝えるためのオプション
+  parserOptions: {
+    ecmaVersion: "latest",  // どのバージョンのECMAScriptの構文を使うかを指定、latestは最新のECMAScriptの構文を使うという意味
+    sourceType: "module",   // JavaScriptコードがどちらのモードで書かれるかを指定するオプション
+  },
+  // 複数のルールを組み合わせてコーディング規約を組み立てる
+  // ルール名: 重大度
+  // off, 	0, 	ルールをオフにする
+  // warn, 	1, 	警告するが終了コードに影響しない
+  // error, 2, 	警告し、終了コードを1にする
+  rules: {
+    "no-console": "error",  
+    camelcase: ["error", { properties: "never" }],
+  },
+};
---
+++ src/helloWorld.js
@@ -0,0 +1,6 @@
+// あえてコード規約を破ったコード
+// export const hello_world = "Hello World";
+// console.log(hello_world);
+
+// 修正したコード
+export const helloWorld = "Hello World";
